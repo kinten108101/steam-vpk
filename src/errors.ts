11c178131cd65b98ce03f85c63bcd8d89c8fa14d
kinten108101 <kinten108101@protonmail.com>
2023-05-16 21:34:50 +0700

    error handling: new error class

---
+++ src/errors.ts
@@ -0,0 +1,43 @@
+type StvpkErrorCode = symbol;
+
+export const Errors = {
+};
+
+interface StvpkErrorCause {
+  readonly code: StvpkErrorCode,
+  value?: unknown,
+}
+
+export interface StvpkErrorConstructor {
+  code: StvpkErrorCode;
+  msg?: string;
+  // TODO: unknown vs any
+  val?: unknown;
+}
+
+// TODO: Prototype instead of class?
+export class StvpkError extends Error {
+  static log(error: StvpkError) {
+    // TODO: log error code
+    log(`Handled StvpkError: ${error.code.toString()}: ${error.message}`);
+  }
+
+  constructor(params: StvpkErrorConstructor = {
+    code: Errors.UNSPECIFIED,
+  }) {
+    const {code, msg, val} = params;
+    super(msg, {
+      cause: {
+        code,
+        val,
+      } as StvpkErrorCause,
+    });
+  }
+
+  get code(): StvpkErrorCode {
+    if (this.cause === null || this.cause === undefined) {
+      throw new TypeError('StvpkError was not properly constructed');
+    }
+    return (<StvpkErrorCause>this.cause).code;
+  }
+}
