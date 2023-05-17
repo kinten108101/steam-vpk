ffebec806f8b98ad0b9494cb529a3acd6d28d696
kinten108101 <kinten108101@protonmail.com>
2023-05-18 01:54:12 +0700

    symbol keys from the global registry

--- src/errors.ts
+++ src/errors.ts
@@ -1,21 +1,21 @@
 type StvpkErrorCode = symbol;
 
 export const Errors = {
-  UNSPECIFIED: Symbol('UNSPECIFIED'),
-  INCLUSION_CHECK_FAILED: Symbol('INCLUSION_CHECK_FAILED'),
-  EMPTY_PROFILE_INDEX: Symbol('EMPTY_PROFILE_INDEX'),
-  INCONSISTENT_FOLDER_NAME: Symbol('INCONSISTENT_FOLDER_NAME'),
-  ADDON_NOT_DOWNLOADED: Symbol('ADDON_NOT_DOWNLOADED'),
-  ADDON_NOT_USED: Symbol('ADDON_NOT_USED'),
-  NO_USABLE_PROFILE: Symbol('NO_USABLE_PROFILE'),
-  SINGLETON_INITIALIZED: Symbol('SINGLETON_INITIALIZED'),
-  SINGLETON_UNINITIALIZED: Symbol('SINGLETON_UNINITIALIZED'),
-  DEPENDENCY_UNINITIALIZED: Symbol('DEPENDENCY_UNINITIALIZED'),
-  UNEXPECTED_TYPE: Symbol('UNEXPECTED_TYPE'),
-  CANCELLED: Symbol('CANCELLED'),
-  PATH_NOT_EXIST: Symbol('PATH_NOT_EXIST'),
-  SETTINGS_NOT_FOUND: Symbol('SETTINGS_NOT_FOUND'),
-  SETTINGS_CANNOT_WRITE: Symbol('SETTINGS_CANNOT_WRITE'),
+  UNSPECIFIED: Symbol.for('UNSPECIFIED'),
+  INCLUSION_CHECK_FAILED: Symbol.for('INCLUSION_CHECK_FAILED'),
+  EMPTY_PROFILE_INDEX: Symbol.for('EMPTY_PROFILE_INDEX'),
+  INCONSISTENT_FOLDER_NAME: Symbol.for('INCONSISTENT_FOLDER_NAME'),
+  ADDON_NOT_DOWNLOADED: Symbol.for('ADDON_NOT_DOWNLOADED'),
+  ADDON_NOT_USED: Symbol.for('ADDON_NOT_USED'),
+  NO_USABLE_PROFILE: Symbol.for('NO_USABLE_PROFILE'),
+  SINGLETON_INITIALIZED: Symbol.for('SINGLETON_INITIALIZED'),
+  SINGLETON_UNINITIALIZED: Symbol.for('SINGLETON_UNINITIALIZED'),
+  DEPENDENCY_UNINITIALIZED: Symbol.for('DEPENDENCY_UNINITIALIZED'),
+  UNEXPECTED_TYPE: Symbol.for('UNEXPECTED_TYPE'),
+  CANCELLED: Symbol.for('CANCELLED'),
+  PATH_NOT_EXIST: Symbol.for('PATH_NOT_EXIST'),
+  SETTINGS_NOT_FOUND: Symbol.for('SETTINGS_NOT_FOUND'),
+  SETTINGS_CANNOT_WRITE: Symbol.for('SETTINGS_CANNOT_WRITE'),
 };
 
 interface StvpkErrorCause {
@@ -26,15 +26,15 @@ interface StvpkErrorCause {
 export interface StvpkErrorConstructor {
   code: StvpkErrorCode;
   msg?: string;
-  // TODO: unknown vs any
   val?: unknown;
 }
 
 // TODO: Prototype instead of class?
 export class StvpkError extends Error {
   static log(error: StvpkError) {
-    // TODO: log error code
-    log(`Handled StvpkError: ${error.code.toString()}: ${error.message}`);
+    const key = Symbol.keyFor(error.code);
+    if (key === undefined) throw new TypeError('StvpkErrorCode was not properly constructed');
+    log(`Handled: ${key}: ${error.message}`);
   }
 
   constructor(params: StvpkErrorConstructor = {
