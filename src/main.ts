b8c892bce6ab9f830233aef8526e6eb418dfe60d
kinten108101 <kinten108101@protonmail.com>
2023-07-14 21:43:39 +0700

    Added new logger module logger.js

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import 'gi://Gdk?version=4.0';
 import 'gi://Gtk?version=4.0';
 import 'gi://Soup?version=3.0';
 
-import { APP_ID } from './const.js';
+import { APP_ID, BUILD_TYPE, BuildTypes } from './const.js';
+import Logger from './logger.js';
+Logger.init({ debug: BUILD_TYPE === BuildTypes.debug });
 
 import { Application } from './application.js';
 
