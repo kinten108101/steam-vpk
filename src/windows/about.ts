bd89824c9d07f0cd9baafc6473aac038f2dcf4c9
kinten108101 <kinten108101@protonmail.com>
2023-09-05 15:11:36 +0700

    Reduced about window

--- src/windows/about.ts
+++ src/windows/about.ts
@@ -10,29 +10,15 @@ export default function AboutWindow(
   const window = new Adw.AboutWindow({
     application_icon: 'addon-box',
     application_name: APP_FULLNAME,
-    developer_name: `Kinten Le`,
     license_type: Gtk.License.GPL_3_0,
     version: VERSION,
     issue_url: 'https://github.com/kinten108101/steam-vpk/issues',
     developers: [ 'Kinten Le <kinten108101@protonmail.com>' ],
     transient_for: parent_window || null,
     website: 'https://github.com/kinten108101/steam-vpk',
-    release_notes: `
-<p>Portals!</p>
-<ul>
-<li>The application should be divided into two components: the front-end (this) and the back-end (Add-on Box). This is so that the async pattern is enforced and so that the project can be extended with external tools and other clients.</li>
-<li>Main window UI renovation.</li>
-</ul>
-`,
-    comments: `<span weight=\'bold\' line_height=\'1.6\'>Philosophy</span>
-Steam VPK is an add-on manager that attempts to replace the Steam Workshop add-on system. Users download the add-ons once (from Steam Workshop, Gamemaps, etc) then control these add-ons on a filesystem level through this application.
+    comments: `Add-on manager for Left 4 Dead 2.
 
-<span weight=\'bold\' line_height=\'1.6\'>The Back-end</span>
-Behind the scene, all data shown on Steam VPK is provided by the Add-on Box daemon.
-
-<span weight=\'bold\' line_height=\'1.6\'>The Name</span>
-<tt>steam-vpk</tt> was an ad-hoc name for a shell-script program I wrote back in 2022, and the name stuck.
-`,
+All data is provided by the Add-on Box daemon.`,
   });
 
   function present() {
