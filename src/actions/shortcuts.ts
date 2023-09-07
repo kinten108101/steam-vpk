1147fa4f16764046d613214e143125dc5b9fcd9f
kinten108101 <kinten108101@protonmail.com>
2023-09-08 00:31:36 +0700

    shortcuts: Use Primary key instead of Control

--- src/actions/shortcuts.ts
+++ src/actions/shortcuts.ts
@@ -9,17 +9,17 @@ export default function Shortcuts(
 }) {
   (<[string, string[]][]>
   [
-    ['app.quit', ['<Control>q']],
-    ['app.new-window', ['<Control>n']],
+    ['app.quit', ['<Primary>q']],
+    ['app.new-window', ['<Primary>n']],
     ['win.reload-addons', ['<Primary>r']],
     ['win.back', ['<Alt>Left']],
     ['win.forward', ['<Alt>Right']],
-    ['win.show-preferences', ['<Control>comma']],
+    ['win.show-preferences', ['<Primary>comma']],
     ['win.stack.page-backward', ['<Shift>Left']],
     ['win.stack.page-forward', ['<Shift>Right']],
-    ['window.close', ['<Control>w']],
-    ['win.add-addon.add-url', ['<Control>b']],
-    ['win.add-addon.add-name', ['<Control>m']],
+    ['window.close', ['<Primary>w']],
+    ['win.add-addon.add-url', ['<Primary>b']],
+    ['win.add-addon.add-name', ['<Primary>m']],
   ]).forEach(([action, accels]) => {
     application.set_accels_for_action(action, accels);
   });
