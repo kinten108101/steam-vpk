fdb8b798aff922a85928fd69e6dc670c6dc4c950
kinten108101 <kinten108101@protonmail.com>
2023-09-14 23:06:39 +0700

    files: bytes unit fix

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,7 +1,7 @@
 export function bytes2humanreadable(bytes: number): string {
   const precision = 3;
   const kbs = bytes / 1000;
-  if (bytes < 1000) return `${bytes.toPrecision(precision)} B`;
+  if (bytes < 1000) return `${bytes} B`;
 
   const mbs = kbs / 1000;
   if (kbs < 1000) return `${kbs.toPrecision(precision)} KB`;
