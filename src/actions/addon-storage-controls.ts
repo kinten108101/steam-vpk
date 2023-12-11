7cb89234c77e3cb1f56dbf4ea8505f0f5a4efcd5
kinten108101 <kinten108101@protonmail.com>
2023-12-11 15:01:27 +0700

    addon-storage-controls: Disused action builder

--- src/actions/addon-storage-controls.ts
+++ src/actions/addon-storage-controls.ts
@@ -4,140 +4,6 @@ import Gio from 'gi://Gio';
 import Adw from 'gi://Adw';
 import AddonBoxClient from '../backend/client.js';
 
-class BuilderPatternError extends Error {
-}
-
-class MissingRequiredParam extends BuilderPatternError {
-  constructor(param_name: string) {
-    super(`Missing required parameter \"${param_name}\"`);
-  }
-}
-
-interface ActivateAsyncSignalCallback {
-    ($obj: Gio.SimpleAction, parameter: GLib.Variant | null): Promise<void>;
-}
-
-class SimpleActionBuilder {
-  parammap: Map<string, any> = new Map;
-  config: {
-    prefix?: string
-  } = {};
-
-  constructor(config: SimpleActionBuilder['config'] = {}) {
-    this.config = config;
-  }
-
-  #get_param<T extends any>(param_name: string): T | undefined {
-    return this.parammap.get(param_name);
-  }
-
-  #get_param_required<T extends any>(param_name: string): T {
-    const param = this.parammap.get(param_name);
-    if (param === undefined) throw new MissingRequiredParam(param_name);
-    return param;
-  }
-
-  name(val: string) {
-    this.parammap.set('name', val);
-    return this;
-  }
-
-  state(val: GLib.Variant): this {
-    this.parammap.set('state', val);
-    return this;
-  }
-
-  #handleTypeSignature(val: GLib.VariantType | string): GLib.VariantType {
-    const type = (() => {
-      if (typeof val === 'string') return GLib.VariantType.new(val);
-      return val;
-    })();
-    return type;
-  }
-
-  state_hint(val: GLib.Variant): this {
-    this.parammap.set('state-hint', val);
-    return this;
-  }
-
-  parameter_type(type: GLib.VariantType): this;
-  parameter_type(type_string: string): this;
-  parameter_type(val: GLib.VariantType | string): this {
-    const type = this.#handleTypeSignature(val);
-    this.parammap.set('parameter-type', type);
-    return this;
-  }
-
-  activate(cb: Gio.SimpleAction.ActivateSignalCallback) {
-    this.parammap.set('activate', cb);
-    this.parammap.set('is-activate-async', false);
-    return this;
-  }
-
-  activate_async(cb: ActivateAsyncSignalCallback) {
-    this.parammap.set('activate', cb);
-    this.parammap.set('is-activate-async', true);
-    return this;
-  }
-
-  change_state(cb: Gio.SimpleAction.ChangeStateSignalCallback) {
-    this.parammap.set('change-state', cb);
-    return this;
-  }
-
-  insert(action_map: Gio.ActionMap) {
-    this.parammap.set('action-map', action_map);
-    return this;
-  }
-
-  build() {
-    const name = (() => {
-      const name = this.#get_param_required<string>('name');
-      const prefix = this.config.prefix;
-      if (prefix !== undefined) return `${prefix}.${name}`;
-      return name;
-    })();
-    const state = this.#get_param<GLib.Variant>('state');
-    const parameter_type = this.#get_param<GLib.VariantType>('parameter-type');
-    const product = new Gio.SimpleAction({ name, parameter_type, state: state || null });
-    const state_hint = this.#get_param<GLib.Variant>('state-hint');
-    if (state_hint !== undefined)
-      product.set_state_hint(state_hint);
-    const activate = this.#get_param<Gio.SimpleAction.ActivateSignalCallback | ActivateAsyncSignalCallback>('activate');
-    if (activate !== undefined) {
-      const is_activate_async = this.#get_param_required<boolean>('is-activate-async');
-      if (is_activate_async) {
-        product.connect('activate', (action, parameter) => {
-          (activate as ActivateAsyncSignalCallback)(action, parameter)
-            .catch(error => logError(error));
-        });
-      }
-      else {
-        product.connect('activate', activate as Gio.SimpleAction.ActivateSignalCallback);
-      }
-    }
-    const change_state = this.#get_param<Gio.SimpleAction.ChangeStateSignalCallback>('change-state');
-    if (change_state !== undefined)
-      product.connect('change-state', change_state);
-    const action_map = this.#get_param<Gio.ActionMap>('action-map');
-    if (action_map !== undefined)
-      action_map.add_action(product);
-    this.reset();
-    return product;
-  }
-
-  reset() {
-    this.parammap.clear();
-    return this;
-  }
-}
-
-export namespace SimpleAction {
-  export function builder(...params: ConstructorParameters<typeof SimpleActionBuilder>) {
-    return new SimpleActionBuilder(...params);
-  }
-}
-
 export default function AddonStorageControls(
 { action_map,
   parent_window,
@@ -147,8 +13,6 @@ export default function AddonStorageControls(
   parent_window?: Gtk.Window;
   client: AddonBoxClient;
 }) {
-  const builder = SimpleAction.builder({ prefix: 'addons' });
-
   const enableAddon = new Gio.SimpleAction({
     name: 'addons.enabled',
     state: GLib.Variant.new_boolean(false),
@@ -189,21 +53,21 @@ export default function AddonStorageControls(
   });
   action_map.add_action(moveUp);
 
-  builder
-    .name('move-down')
-    .parameter_type('s')
-    .activate((_, parameter) => {
-      if (parameter === null) throw new Error;
-      const [id] = parameter.get_string();
-      if (id === null) throw new Error;
-    })
-    .insert(action_map)
-    .build();
+  const moveDown = new Gio.SimpleAction({
+    name: 'addons.move-down',
+  });
+  moveDown.connect('activate', (_action, parameter) => {
+    if (parameter === null) throw new Error;
+    const [id] = parameter.get_string();
+    if (id === null) throw new Error;
+  });
+  action_map.add_action(moveDown);
 
-  builder
-    .name('remove')
-    .parameter_type('s')
-    .activate_async(async (_action, parameter) => {
+  const remove = new Gio.SimpleAction({
+    name: 'addons.remove',
+  });
+  remove.connect('activate', (_action, parameter) => {
+    (async () => {
       if (parameter === null) throw new Error;
       const [id] = parameter.get_string();
       if (id === null) throw new Error;
@@ -224,57 +88,56 @@ export default function AddonStorageControls(
       if (response !== 'remove.proceed') {
         console.warn(`Received unknown response in removeAddon. Got ${response}`);
       }
-    })
-    .insert(action_map)
-    .build();
+    })().catch(logError);
+  });
+  action_map.add_action(remove);
 
-  builder
-    .name('active')
-    .parameter_type(GLib.VariantType.new_tuple([new GLib.VariantType('s'), new GLib.VariantType('b')]))
-    .activate((_, parameter) => {
-      if (!(parameter instanceof GLib.Variant)) throw new Error(`Expect a GVariant, got ${parameter}`);
-      const [id, active] = parameter.deepUnpack() as Array<any>;
-      if (typeof id !== 'string' || typeof active !== 'boolean') {
-        throw new Error(`Expected (sb), got ${id} and ${active}`);
-      }
+  const activate = new Gio.SimpleAction({
+    name: 'addons.activate',
+  });
+  activate.connect('activate', (_action, parameter) => {
+    if (!(parameter instanceof GLib.Variant)) throw new Error(`Expect a GVariant, got ${parameter}`);
+    const [id, active] = parameter.deepUnpack() as Array<any>;
+    if (typeof id !== 'string' || typeof active !== 'boolean') {
+      throw new Error(`Expected (sb), got ${id} and ${active}`);
+    }
+  });
+  action_map.add_action(activate);
 
-    })
-    .insert(action_map)
-    .build();
-
-  builder
-    .name('trash')
-    .parameter_type('s')
-    .activate_async(async (_action, parameter) => {
-      if (parameter === null) throw new Error;
-      const [id] = parameter.get_string();
-      if (id === null) throw new Error;
-      const msg = new Adw.MessageDialog() as ({
-        choose_async: (cancellable: Gio.Cancellable | null) => Promise<string>;
-      } & Adw.MessageDialog);
-      msg.set_heading('Delete this add-on?');
-      msg.set_body('Deleted content is recoverable from trash can.');
-      msg.add_response('trash.cancel', 'Cancel');
-      msg.add_response('trash.proceed', 'Proceed');
-      msg.set_response_appearance('trash.proceed', Adw.ResponseAppearance.DESTRUCTIVE);
-      msg.set_close_response('trash.cancel');
-      msg.set_default_response('trash.proceed');
-      msg.set_transient_for(parent_window || null);
-      const response = await msg.choose_async(null)
-      switch (response) {
-      case 'trash.cancel':
-        return;
-      case 'trash.proceed':
-        break;
-      default:
-        throw new Error;
-      }
-      try {
-        await client.services.addons.async_call('Delete', id)
-      } catch (error) {
-        logError(error);
-      }
-    })
-    .insert(action_map)
-    .build();
+  const trash = new Gio.SimpleAction({
+    name: 'addons.trash',
+  });
+  trash.connect('activate', (_action, parameter) => {
+    (async () => {
+        if (parameter === null) throw new Error;
+        const [id] = parameter.get_string();
+        if (id === null) throw new Error;
+        const msg = new Adw.MessageDialog() as ({
+          choose_async: (cancellable: Gio.Cancellable | null) => Promise<string>;
+        } & Adw.MessageDialog);
+        msg.set_heading('Delete this add-on?');
+        msg.set_body('Deleted content is recoverable from trash can.');
+        msg.add_response('trash.cancel', 'Cancel');
+        msg.add_response('trash.proceed', 'Proceed');
+        msg.set_response_appearance('trash.proceed', Adw.ResponseAppearance.DESTRUCTIVE);
+        msg.set_close_response('trash.cancel');
+        msg.set_default_response('trash.proceed');
+        msg.set_transient_for(parent_window || null);
+        const response = await msg.choose_async(null)
+        switch (response) {
+        case 'trash.cancel':
+          return;
+        case 'trash.proceed':
+          break;
+        default:
+          throw new Error;
+        }
+        try {
+          await client.services.addons.async_call('Delete', id)
+        } catch (error) {
+          logError(error);
+        }
+    })().catch(logError);
+  });
+  action_map.add_action(trash);
 }
